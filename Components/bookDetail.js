1dde79fdf88987aa3fadd496f07e3f3afa2eedb2
Majid Laso <lasomajid@gmail.com>
2022-11-24 10:47:23 +0100

    intro

---
+++ Components/bookDetail.js
@@ -0,0 +1,9 @@
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Titel</div>
+          <p class="text-gray-700 text-base">
+                Information om boken du sökt på: 
+          </p>
+        </div>
+        <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+    </div>
